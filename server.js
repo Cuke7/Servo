194c0679980c890467f3a076f9dcd6e70ebcdb55
Cuke7 <cassany.louis@gmail.com>
2021-03-29 10:20:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// http-server -p8000 -c-1
+
+const express = require("express");
+const app = express();
+var router = express.Router();
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Listening to requests on" + process.env.PORT);
+});
+
+const sendSeekable = require("send-seekable");
+app.use(sendSeekable);
+
+app.use("/", require("./API"));
+app.use(express.static("public"));
