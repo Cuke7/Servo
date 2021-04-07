7871809475a454cd52198f152e4797347243c485
Cuke7 <cuke.dev@mail.com>
2021-04-07 15:50:25 +0200

    YES

--- API/monsterAPI.js
+++ API/monsterAPI.js
@@ -14,6 +14,7 @@ router.route("/get_rencontre").get(get_rencontre);
 async function get_rencontre(req, resp) {
     // http://192.168.1.18:8000/monsterAPI/get_rencontre?FPmin=0&FPmax=30&alignement=AA&number=5&types=Aberration,Aberration%20(m%C3%A9tamorphe),B%C3%AAte
 
+    ///monsterAPI/get_rencontre?FPmin=1/2&FPmax=9&alignement=AA&types=&noms=Aboleth,Allosaure&number=3&ident=false
     // CORS STUFF
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
@@ -34,25 +35,28 @@ async function get_rencontre(req, resp) {
     let ident = decodeURI(req.query.ident) == "undefined" ? "false" : decodeURI(req.query.ident);
     let types = decodeURI(req.query.types) == "undefined" ? [] : decodeURI(req.query.types);
     types = types.split(",");
-    if(types[0] === ''){
+    if (types[0] === "") {
         types = [];
+    }
+    let noms = decodeURI(req.query.noms) == "undefined" ? [] : decodeURI(req.query.noms);
+    noms = noms.split(",");
+    if (noms[0] === "") {
+        noms = [];
     }
 
     let candidates = [];
 
-    console.log(FPmin, FPmax, number, alignement, ident, types);
+    console.log(FPmin, FPmax, number, alignement, ident, types, noms);
 
     for (const monster of monsters) {
         if (eval(monster.FP) <= FPmax) {
             if (eval(monster.FP) >= FPmin) {
                 if (alignement[0] == monster.alignement[0] || alignement[0] == "A") {
                     if (alignement[1] == monster.alignement[1] || alignement[1] == "A") {
-                        if (types.length > 0) {
-                            if (types.includes(monster.type)) {
-                                candidates.push(monster);
+                        if (types.includes(monster.type) || types.length == 0) {
+                            if (noms.includes(monster.nom) || noms.length == 0) {
+                                candidates.push([monster.nom, monster.type]);
                             }
-                        } else {
-                            candidates.push(monster);
                         }
                     }
                 }
